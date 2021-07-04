c846d6c269a73ddbc2b353f3060ea3b08e4a7f06
yash-learner <yash.topaz@gmail.com>
2021-07-04 18:59:44 +0530

    Created index, create and update methods in menu_categories_controller.rb

--- app/controllers/menu_categories_controller.rb
+++ app/controllers/menu_categories_controller.rb
@@ -1,2 +1,25 @@
-class MenuCategoryController < ApplicationController
+class MenuCategoriesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  def index
+    render plain:"menu items"
+  end
+
+  def create
+    category_name = params[:category_name]
+    new_category  = MenuCategory.new(
+      name: category_name
+    )
+    if new_category.save
+      render plain: "new category has created!"
+    end
+  end
+
+  def update
+    id = params[:id]
+    category_name = params[:category_name]
+    category = MenuCategory.find(id)
+    category.name = category_name
+    category.save!
+    render plain: "category name has been updated."
+  end
 end
