c068b1604f43518cc199915c4146c5ad31a97883
yash-learner <yash.topaz@gmail.com>
2021-07-13 00:40:32 +0530

    Now User can place an order After order button is clicked the order id is generated and all the items in cart is moved to order and then all the items in cart are removed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,23 @@
 class OrdersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :ensure_user_logged_in
+
   def index
     cart = current_user.cart
     @cart_items = CartItem.where("cart_id = ?", cart)
     render "index"
+  end
+
+  def create
+    order = Order.new(
+      date: Date.today,
+      user_id: current_user.id,
+      delivered_at: false,
+    )
+    if order.save
+      # session[:current_order_id] = order.id
+
+      redirect_to "/order_items/#{order.id}/order"
+    end
   end
 end
