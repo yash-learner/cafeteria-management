4c533a3e71b33b15cc228facc3fa2f927f2e3411
yash-learner <yash.topaz@gmail.com>
2021-07-15 13:55:40 +0530

    fixed problems

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,6 +2,14 @@ class OrderItemsController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :ensure_user_logged_in
 
+  def index
+    # @orders = current_user.orders.pluck(:id)
+    # @orders = OrderItem.where(:order_id => current_user.orders.ids)
+    @orders = OrderItem.where("order_id IN (?)", current_user.orders.ids)
+
+    render "index"
+  end
+
   def order
     CartItem.all.each do |item|
       order_item = OrderItem.new(
@@ -15,6 +23,7 @@ class OrderItemsController < ApplicationController
       order_item.save
     end
     if CartItem.all.count == CartItem.all.count
+      flash[:error] = "Your Order has been placed!"
       logger.info "Successfully placed order"
       CartItem.where("cart_id = ?", current_user.cart).destroy_all
       redirect_to "/menu_categories"
